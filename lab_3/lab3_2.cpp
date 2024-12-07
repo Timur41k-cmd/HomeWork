e132be192aeb6f0222c77d00b4a7ba3f2681b66f
Timur41k-cmd <cool.jatik@gmail.com>
2024-12-07 19:47:41 +0300

    lab3 fixed

--- lab_3/lab3_2.cpp
+++ lab_3/lab3_2.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 int main()
 {
+string res_anim,res_color;
 string anim_l[12] =
 {"крыса","корова","тигр","заяц","дракон","змея","лошадь","овца","обезьяна","курица","собака","свинья"};
-string color_l[6] = {"зеленый", "красный","желтый", "белый", "черный"};
+string color_l[5] = {"зеленый", "красный","желтый", "белый", "черный"};
 int year,color, anim;
 cout << "Японский календарь" << endl << "Введите год:"<< endl;
 cin >> year;
@@ -16,5 +17,48 @@ year = (year -1984);
 }
 color = ((year % 60) / 12);
 anim = ((year%60))%12;
-cout<< color_l[color] << " " << anim_l[anim];
+switch(color){
+case 0:
+res_color = "зеленый";
+case 2:
+res_color = "красный";
+case 3:
+res_color = "желтый";
+case 4:
+res_color = "белый";
+case 5:
+res_color = "черный";
+}
+
+switch(anim){
+case 0:
+res_anim = anim_l[0];
+case 1:
+res_anim = anim_l[1];
+case 2:
+res_anim = anim_l[2];
+case 3:
+res_anim = anim_l[3];
+case 4:
+res_anim = anim_l[4];
+case 5:
+res_anim = anim_l[5];
+case 6:
+res_anim = anim_l[6];
+case 7:
+res_anim = anim_l[7];
+case 8:
+res_anim = anim_l[8];
+case 9:
+res_anim = anim_l[9];
+case 10:
+res_anim = anim_l[10];
+case 11:
+res_anim = anim_l[11];
+
+
+
+}
+
+
 }
